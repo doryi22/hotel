4474d6fe8a61d6cf266c418aaa63c754eabe1a1f
doryi22 <up202305149@up.pt>
2024-05-16 11:56:31 +0100

    classe cliente

--- classes/cliente.py
+++ classes/cliente.py
@@ -8,17 +8,16 @@ class Cliente(Gclass):
     pos = 0
     sortkey = ''
     auto_number = 0
-    att = ['_client_code','_name', '_npessoas', '_dob', '_idade']
+    att = ['_client_code','_name', '_dob', '_idade']
     header = 'Cliente'
-    des = ['Client Code','Name','NPessoas','Date of Birth','Idade']
+    des = ['Client Code','Name','Date of Birth','Idade']
 
-    def __init__(self, client_code, name, npessoas, dob, idade=None):
+    def __init__(self, client_code, name, dob, idade=None):
         super().__init__()
         self._client_code = client_code
         self._name = name
         self._dob = dob
         self._idade = None
-        self._npessoas = npessoas
         Cliente.obj[client_code] = self
         Cliente.lst.append(client_code)
         
